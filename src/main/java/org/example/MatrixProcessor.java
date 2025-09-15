2ee64ce7e288631e27bf6461863c963e87846961
dk872 <dimakulyk2005@gmail.com>
2025-09-15 16:52:03 +0300

    Add a function to multiply a matrix by a number

---
+++ src/main/java/org/example/MatrixProcessor.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class MatrixProcessor {
+
+    public static void main(String[] args) {
+    }
+
+    public static byte[][] multiplyMatrix(byte[][] matrix, byte multiplier) {
+        byte[][] result = new byte[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int value = multiplier * matrix[i][j];
+
+                if (value > Byte.MAX_VALUE || value < Byte.MIN_VALUE) {
+                    throw new ArithmeticException("Byte overflow during multiplication");
+                }
+
+                result[i][j] = (byte) value;
+            }
+        }
+        return result;
+    }
+}
